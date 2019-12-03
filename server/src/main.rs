78d1fb19e6b130793cae1ff793be762539977b52
Alex Kornitzer <alex.kornitzer@countercept.com>
2019-12-03 10:25:10 +0000

    nng: nng based linking error

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,38 +1,15 @@
 #[macro_use]
 extern crate shared;
 
-use std::convert::TryFrom;
 use std::time::Duration;
 
 use shared::nng::options::{Options, SendTimeout};
-use shared::nng::{Message, Protocol, Socket};
-use shared::{CommandRequest, CommandResponse};
+use shared::nng::{Protocol, Socket};
 
 fn main() {
-    info!("i am the server");
     let socket = Socket::new(Protocol::Rep0).unwrap();
     socket
         .set_opt::<SendTimeout>(Some(Duration::from_secs(60)))
         .unwrap();
     socket.listen(&format!("ipc:///tmp/dylibs")).unwrap();
-
-    loop {
-        match socket.recv() {
-            Ok(request) => {
-                let request = CommandRequest::try_from(request).unwrap();
-                process(request);
-                let response = Message::try_from(CommandResponse::Ack).unwrap();
-                if let Err(err) = socket.send(response) {
-                    error!("failed to send response: {:?}", err);
-                }
-            }
-            Err(err) => {
-                error!("error receiving: {:?}", err);
-            }
-        }
-    }
-}
-
-fn process(request: CommandRequest) {
-    info!("processing request: {:?}", request);
 }
